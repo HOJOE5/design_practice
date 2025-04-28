c94eafb198ecefc7f0ae95aaf3713339af49c5a9
HOJOE5 <eddywell69@gmail.com>
2025-04-28 13:06:33 +0900

    1차 디자인 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart'; // ❗ 가장 위에 추가
+import 'package:flutter/material.dart'; // 가장 위에 추가
 
 void main() {
-  runApp(MyApp()); // ❗ 무조건 runApp(MyApp())
+  runApp(MyApp()); // 무조건 runApp(MyApp())
 }
 
 class MyApp extends StatelessWidget {
-  // ❗ Flutter 기본 구조
+  // Flutter 기본 구조
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,17 +13,59 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text('앱 제목')),
         body: Column(
           children: [
+            SizedBox(height: 20), // 위에 간격
             Container(
-              color: Colors.blue,
-              height: 100,
-              width: double.infinity,
-              child: Center(child: Text('첫 번째 박스')),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.blue[300],
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  '첫 번째 박스',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
+            SizedBox(height: 20), // 박스 간격
             Container(
-              color: Colors.green,
-              height: 100,
-              width: double.infinity,
-              child: Center(child: Text('두 번째 박스')),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.green[300],
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  '두 번째 박스',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
